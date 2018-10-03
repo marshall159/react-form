88c50f306ada0e42d89516e319def61a47a39056
Aneel Marshall <aneel.marshall@outlook.com>
2018-10-03 15:43:22 +0100

    feat: add removeCharacter method

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import Table from './Table';
 
 class App extends Component {
-    render() {
-        const characters = [
+    state = {
+        characters: [
             {
                 'name': 'Charlie',
                 'job': 'Janitor'
@@ -20,7 +20,21 @@ class App extends Component {
                 'name': 'Dennis',
                 'job': 'Bartender'
             }
-        ];
+        ]
+    };
+
+    removeCharacter = (index) => {
+        const { characters } = this.state;
+
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i !== index;
+            })
+        });
+       
+    }
+
+    render() {
 
         return(
             <div className="container">
