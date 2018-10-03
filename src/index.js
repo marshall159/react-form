3d1d9ea12ee9918d813d541706c24772b79ff8f8
Aneel Marshall <aneel.marshall@outlook.com>
2018-10-03 12:03:48 +0100

    refactor: extract App component into a file

--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-class App extends Component {
-    render() {
-        return(
-            <div>
-                <h1>Hello World</h1>
-            </div>
-        )
-    }
-}
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
