f18c718c624b6e664aa10ff7556ad3da69e1c63b
Aneel Marshall <aneel.marshall@outlook.com>
2018-10-03 11:52:55 +0100

    feat: add hello world component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+class App extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Hello World</h1>
+            </div>
+        )
+    }
+}
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
